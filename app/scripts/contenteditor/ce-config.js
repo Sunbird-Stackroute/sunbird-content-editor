c8d0b39c3e0d89ee39333ccc911476168e850508
Uma536 <55277404+Uma536@users.noreply.github.com>
2021-05-11 19:39:56 +0530

    added zoom plugin in ce-config.js file

--- app/scripts/contenteditor/ce-config.js
+++ app/scripts/contenteditor/ce-config.js
@@ -13,7 +13,8 @@ org.ekstep.contenteditor.config = {
 		{ 'id': 'org.ekstep.preview', 'ver': '1.1', 'type': 'plugin' },
 		{ 'id': 'org.ekstep.editcontentmeta', 'ver': '1.1', 'type': 'plugin' },
 		{ 'id': 'org.ekstep.quiz', 'ver': '1.1', 'type': 'plugin' },
-		{ 'id': 'org.ekstep.video', 'ver': '1.0', 'type': 'plugin' }
+		{ 'id': 'org.ekstep.video', 'ver': '1.0', 'type': 'plugin' },
+		{ 'id': 'org.ekstep.zoom.video', 'ver': '1.0', 'type': 'plugin' }
 	],
 	corePluginsPackaged: true,
 	dispatcher: 'local',
