2cffeb9d2b8402801f999fad7aca8101037fd056
Uma536 <55277404+Uma536@users.noreply.github.com>
2021-05-11 16:45:35 +0530

    configuration changes done for zoom Integration

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -22,7 +22,7 @@ module.exports = function(app, dirname) {
 	});
 
 	app.post('/app/telemetry', function(req, res) {
-		fs.appendFile('telemetry.json', req.body.event + "\n");
+		//fs.appendFile('telemetry.json', req.body.event + "\n");
 		res.end();
 	});
 };
